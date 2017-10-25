9938c1e0cafd05fbfe8255234b7e3c8502492b6f
Josue Perez <che.perez3@gmail.com>
2017-10-24 22:31:32 -0400

    Set up cofig.js and server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+const app = express();
+
+app.use(methodOverride('_method'));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static('public'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+  console.log(`knock knock ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+//const todoRoutes = require('./routes/todo-routes')
+//app.use('/todo', todoRoutes);
+
+app.get('*', (req, res) => {
+ res.status(404).send('404');
+});
